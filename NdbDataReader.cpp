ed497ecdd89845bf1db72321f3ef0c2b26e2fb9f
Mahmoud Ismail <maism@hops.io>
2016-04-15 18:38:39 +0200

    test the whole pipline for inodes

--- NdbDataReader.cpp
+++ NdbDataReader.cpp
@@ -24,6 +24,12 @@
 
 #include "NdbDataReader.h"
 #include "Utils.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <boost/network.hpp>
+
+using namespace boost::network;
+using namespace boost::network::http; 
 
 NdbDataReader::NdbDataReader(Ndb** connections, const int num_readers) : mNdbConnections(connections), mNumReaders(num_readers){
     mStarted = false;
@@ -99,20 +105,100 @@ void NdbDataReader::readData(Ndb* connection, Cus* added) {
     if(ts->execute(NdbTransaction::Commit) == -1){
         LOG_NDB_API_ERROR(ts->getNdbError());
     }
-    
+
+
+    std::stringstream out;
+
     for (int i = 0; i < batch_size; i++) {
-          for(int c=0; c<num_inodes_columns; c++){
-              NdbRecAttr* col = cols[i][c];
-              if(c == 0 && pending[i].mInodeId != col->int32_value()){
-                   LOG_INFO() << " Data for " << pending[i].mParentId << ", " << pending[i].mInodeName << " not found";
-                  break;
-              }
-            LOG_INFO() << "Got values for INode " << col->getColumn()->getName() << " = "<< col->int64_value(); 
-          }
+        if (pending[i].mInodeId != cols[i][0]->int32_value()) {
+            LOG_INFO() << " Data for " << pending[i].mParentId << ", " << pending[i].mInodeName << " not found";
+            break;
+        }
+
+        for (int c = 0; c < num_inodes_columns; c++) {
+            NdbRecAttr* col = cols[i][c];
+            LOG_INFO() << "Got values for INode " << col->getColumn()->getName() << " = " << col->int64_value();
+        }
+
+
+        rapidjson::StringBuffer sbOp;
+        rapidjson::Writer<rapidjson::StringBuffer> opWriter(sbOp);
+
+        opWriter.StartObject();
+        
+        opWriter.String("update");
+        opWriter.StartObject();
+
+        opWriter.String("_index");
+        opWriter.String("test");
+        
+        opWriter.String("_type");
+        opWriter.String("inodes");
+        
+        opWriter.String("_id");
+        opWriter.Int(cols[i][0]->int32_value());
+
+        opWriter.EndObject();
+        
+        opWriter.EndObject();
+        
+        out << sbOp.GetString() << endl;
+        
+        rapidjson::StringBuffer sbDoc;
+        rapidjson::Writer<rapidjson::StringBuffer> docWriter(sbDoc);
+        
+        docWriter.StartObject();
+        docWriter.String("doc");
+        docWriter.StartObject();
+        
+        docWriter.String("parent_id");
+        docWriter.Int(pending[i].mParentId);
+        
+        docWriter.String("name");
+        docWriter.String(pending[i].mInodeName.c_str());
+        
+        docWriter.String("operation");
+        docWriter.Int(pending[i].mOperation);
+        
+        docWriter.String("timestamp");
+        docWriter.Int64(pending[i].mTimestamp);
+        
+        docWriter.String("size");
+        docWriter.Int64(cols[i][1]->int64_value());
+        
+        docWriter.EndObject();
+        
+        docWriter.String("doc_as_upsert");
+        docWriter.Bool(true);
+        
+        docWriter.EndObject();
+        
+        out << sbDoc.GetString() << endl;
+                
     }
+
+    std::string data = out.str();
+        
+    LOG_INFO() << " Out :: " << endl << data << endl;
 
     connection->closeTransaction(ts);
     
+    
+    client::request request_("http://localhost:9200/_bulk");
+    request_ << header("Connection", "close");
+    request_ << header("Content-Type", "application/json");
+    
+    char body_str_len[8];
+    sprintf(body_str_len, "%lu", data.length());
+
+    request_ << header("Content-Length", body_str_len);
+    request_ << body(data);
+    
+    client client_;
+    client::response response_ = client_.post(request_);
+    std::string body_ = body(response_);
+
+    LOG_INFO() << body_;
 }
 
 void NdbDataReader::process_batch(Cus_Cus added_deleted_batch) {
