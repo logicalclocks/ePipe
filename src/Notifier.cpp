304ec5b58dfae8eb23b1caa22d63d08464b874ae
Mahmoud Ismail <maism@hops.io>
2018-05-17 11:44:57 +0200

    [HOPSWORKS-564] Refactoring ePipe to easily enable or disable different watch units

--- src/Notifier.cpp
+++ src/Notifier.cpp
@@ -26,16 +26,16 @@
 #include "ProvenanceBatcher.h"
 
 Notifier::Notifier(const char* connection_string, const char* database_name, const char* meta_database_name,
-        const TableUnitConf mutations_tu, const TableUnitConf metadata_tu, const TableUnitConf schemaless_tu, TableUnitConf provenance_tu,
+        const TableUnitConf mutations_tu, const TableUnitConf schemabased_tu, const TableUnitConf schemaless_tu, TableUnitConf provenance_tu,
         const int poll_maxTimeToWait, const string elastic_ip, const bool hopsworks, const string elastic_index, const string elastic_provenance_index,
         const string elasttic_project_type, const string elastic_dataset_type, const string elastic_inode_type, const string elastic_provenance_type,
         const int elastic_batch_size, const int elastic_issue_time, const int lru_cap, const bool recovery, 
-        const bool stats, MetadataType metadata_type, Barrier barrier)
-: mDatabaseName(database_name), mMetaDatabaseName(meta_database_name), mMutationsTU(mutations_tu), mMetadataTU(metadata_tu), 
+        const bool stats, Barrier barrier)
+: mDatabaseName(database_name), mMetaDatabaseName(meta_database_name), mMutationsTU(mutations_tu), mSchemabasedTU(schemabased_tu), 
         mSchemalessTU(schemaless_tu), mProvenanceTU(provenance_tu), mPollMaxTimeToWait(poll_maxTimeToWait), mElasticAddr(elastic_ip), mHopsworksEnabled(hopsworks),
         mElasticIndex(elastic_index), mElasticProvenanceIndex(elastic_provenance_index), mElastticProjectType(elasttic_project_type), mElasticDatasetType(elastic_dataset_type), 
         mElasticInodeType(elastic_inode_type), mElasticProvenanceType(elastic_provenance_type), mElasticBatchsize(elastic_batch_size), mElasticIssueTime(elastic_issue_time), mLRUCap(lru_cap), 
-        mRecovery(recovery), mStats(stats), mMetadataType(metadata_type), mBarrier(barrier) {
+        mRecovery(recovery), mStats(stats), mBarrier(barrier) {
     mClusterConnection = connect_to_cluster(connection_string);
     setup();
 }
@@ -44,95 +44,127 @@ void Notifier::start() {
     LOG_INFO("ePipe starting...");
     ptime t1 = getCurrentTime();
     
-    mFsMutationsDataReader->start();
-    if(mMetadataType == Schemabased || mMetadataType == Both){
-        mSchemabasedMetadataReader->start();
+    if(mMutationsTU.isEnabled()){
+        mFsMutationsDataReader->start();
+        mFsMutationsBatcher->start();
+        mFsMutationsTableTailer->start(mRecovery);
     }
-    if(mMetadataType == Schemaless || mMetadataType == Both){
+    
+    if(mSchemabasedTU.isEnabled()){
+        mSchemabasedMetadataReader->start();
+        mSchemabasedMetadataBatcher->start();
+        mMetadataLogTailer->start(mRecovery);
+    }
+    
+    if(mSchemalessTU.isEnabled()){
         mSchemalessMetadataReader->start();
-    }    
-    mElasticSearch->start();
+        mSchemalessMetadataBatcher->start();
+        mMetadataLogTailer->start(mRecovery);
+    }
+    
+    if(mMutationsTU.isEnabled() || mSchemabasedTU.isEnabled() 
+            || mSchemalessTU.isEnabled() || mHopsworksEnabled){
+        mProjectsElasticSearch->start();
+    }
        
     if (mHopsworksEnabled) {
         mhopsworksOpsLogTailer->start(mRecovery);
     }
     
-    mFsMutationsBatcher->start();
-    mFsMutationsTableTailer->start(mRecovery);
-    
-    if(mMetadataType == Schemabased || mMetadataType == Schemaless || mMetadataType == Both){
-        mMetadataLogTailer->start(mRecovery);
+    if(mProvenanceTU.isEnabled()){
+        mProvenancElasticSearch->start();
+        mProvenanceDataReader->start();
+        mProvenanceBatcher->start();
+        mProvenanceTableTailer->start(mRecovery);
     }
-    
-    if (mMetadataType == Schemabased || mMetadataType == Both) {
-        mSchemabasedMetadataBatcher->start();
-    }
-
-    if (mMetadataType == Schemaless || mMetadataType == Both) {
-        mSchemalessMetadataBatcher->start();
-    }
-    
-    mProvenancElasticSearch->start();
-    
-    mProvenanceDataReader->start();
-    mProvenanceBatcher->start();
-    mProvenanceTableTailer->start(mRecovery);
     
     ptime t2 = getCurrentTime();
     LOG_INFO("ePipe started in " << getTimeDiffInMilliseconds(t1, t2) << " msec");
-    mFsMutationsBatcher->waitToFinish();
-    mSchemabasedMetadataBatcher->waitToFinish();
-    mElasticSearch->waitToFinish();
     
-    mProvenanceBatcher->waitToFinish();
-    mProvenancElasticSearch->waitToFinish();
+    if(mMutationsTU.isEnabled()){
+        mFsMutationsBatcher->waitToFinish();
+        mFsMutationsTableTailer->waitToFinish();
+    }
+    
+    if(mSchemabasedTU.isEnabled()){
+        mSchemabasedMetadataBatcher->waitToFinish();
+        mMetadataLogTailer->waitToFinish();
+    }
+   
+    if(mSchemalessTU.isEnabled()){
+        mSchemalessMetadataBatcher->waitToFinish();
+        mMetadataLogTailer->waitToFinish();
+    }
+    
+    if(mMutationsTU.isEnabled() || mSchemabasedTU.isEnabled()
+            || mSchemalessTU.isEnabled() || mHopsworksEnabled){
+        mProjectsElasticSearch->waitToFinish();
+    }
+    
+    if (mHopsworksEnabled) {
+        mhopsworksOpsLogTailer->waitToFinish();
+    }
+    
+    if(mProvenanceTU.isEnabled()){
+        mProvenanceBatcher->waitToFinish();
+        mProvenanceTableTailer->waitToFinish();
+        mProvenancElasticSearch->waitToFinish();
+    }
 }
 
 void Notifier::setup() {
     mPDICache = new ProjectDatasetINodeCache(mLRUCap);
     mSchemaCache = new SchemaCache(mLRUCap);
-    
-    MConn ndb_connections_elastic;
-    ndb_connections_elastic.metadataConnection = create_ndb_connection(mMetaDatabaseName);
-    ndb_connections_elastic.inodeConnection = create_ndb_connection(mDatabaseName);
-    
-    mElasticSearch = new ProjectsElasticSearch(mElasticAddr, mElasticIndex, mElastticProjectType,
-            mElasticDatasetType, mElasticInodeType, mElasticIssueTime, mElasticBatchsize, 
-            mStats, ndb_connections_elastic);
-    
-    Ndb* mutations_tailer_connection = create_ndb_connection(mDatabaseName);
-    mFsMutationsTableTailer = new FsMutationsTableTailer(mutations_tailer_connection, mPollMaxTimeToWait, mBarrier, mPDICache);
-    
-    MConn* mutations_connections = new MConn[mMutationsTU.mNumReaders];
-    for(int i=0; i< mMutationsTU.mNumReaders; i++){
-        mutations_connections[i].inodeConnection =  create_ndb_connection(mDatabaseName);
-        mutations_connections[i].metadataConnection = create_ndb_connection(mMetaDatabaseName);
+
+    if (mMutationsTU.isEnabled() || mSchemabasedTU.isEnabled()
+            || mSchemalessTU.isEnabled() || mHopsworksEnabled) {
+        MConn ndb_connections_elastic;
+        ndb_connections_elastic.metadataConnection = create_ndb_connection(mMetaDatabaseName);
+        ndb_connections_elastic.inodeConnection = create_ndb_connection(mDatabaseName);
+
+        mProjectsElasticSearch = new ProjectsElasticSearch(mElasticAddr, mElasticIndex, mElastticProjectType,
+                mElasticDatasetType, mElasticInodeType, mElasticIssueTime, mElasticBatchsize,
+                mStats, ndb_connections_elastic);
     }
-    
-    mFsMutationsDataReader = new FsMutationsDataReader(mutations_connections, mMutationsTU.mNumReaders, 
-           mHopsworksEnabled, mElasticSearch, mPDICache, mLRUCap);
-    mFsMutationsBatcher = new FsMutationsBatcher(mFsMutationsTableTailer, mFsMutationsDataReader, 
-            mMutationsTU.mWaitTime, mMutationsTU.mBatchSize);
-    
+  
 
-    Ndb* metadata_tailer_connection = create_ndb_connection(mMetaDatabaseName);
-    mMetadataLogTailer = new MetadataLogTailer(metadata_tailer_connection, mPollMaxTimeToWait, mBarrier);
-    
-    if (mMetadataType == Schemabased || mMetadataType == Both) {
+    if (mMutationsTU.isEnabled()) {
+        Ndb* mutations_tailer_connection = create_ndb_connection(mDatabaseName);
+        mFsMutationsTableTailer = new FsMutationsTableTailer(mutations_tailer_connection, mPollMaxTimeToWait, mBarrier, mPDICache);
 
-        MConn* metadata_connections = new MConn[mMetadataTU.mNumReaders];
-        for (int i = 0; i < mMetadataTU.mNumReaders; i++) {
+        MConn* mutations_connections = new MConn[mMutationsTU.mNumReaders];
+        for (int i = 0; i < mMutationsTU.mNumReaders; i++) {
+            mutations_connections[i].inodeConnection = create_ndb_connection(mDatabaseName);
+            mutations_connections[i].metadataConnection = create_ndb_connection(mMetaDatabaseName);
+        }
+
+        mFsMutationsDataReader = new FsMutationsDataReader(mutations_connections, mMutationsTU.mNumReaders,
+                mHopsworksEnabled, mProjectsElasticSearch, mPDICache, mLRUCap);
+        mFsMutationsBatcher = new FsMutationsBatcher(mFsMutationsTableTailer, mFsMutationsDataReader,
+                mMutationsTU.mWaitTime, mMutationsTU.mBatchSize);
+    }
+   
+    
+    if(mSchemabasedTU.isEnabled() || mSchemalessTU.isEnabled()){
+        Ndb* metadata_tailer_connection = create_ndb_connection(mMetaDatabaseName);
+        mMetadataLogTailer = new MetadataLogTailer(metadata_tailer_connection, mPollMaxTimeToWait, mBarrier);
+    }
+        
+    if (mSchemabasedTU.isEnabled()) {
+
+        MConn* metadata_connections = new MConn[mSchemabasedTU.mNumReaders];
+        for (int i = 0; i < mSchemabasedTU.mNumReaders; i++) {
             metadata_connections[i].inodeConnection = create_ndb_connection(mDatabaseName);
             metadata_connections[i].metadataConnection = create_ndb_connection(mMetaDatabaseName);
         }
 
-        mSchemabasedMetadataReader = new SchemabasedMetadataReader(metadata_connections, mMetadataTU.mNumReaders,
-                mHopsworksEnabled, mElasticSearch, mPDICache, mSchemaCache);
+        mSchemabasedMetadataReader = new SchemabasedMetadataReader(metadata_connections, mSchemabasedTU.mNumReaders,
+                mHopsworksEnabled, mProjectsElasticSearch, mPDICache, mSchemaCache);
         mSchemabasedMetadataBatcher = new SchemabasedMetadataBatcher(mMetadataLogTailer, mSchemabasedMetadataReader,
-                mMetadataTU.mWaitTime, mMetadataTU.mBatchSize);
+                mSchemabasedTU.mWaitTime, mSchemabasedTU.mBatchSize);
     }
         
-    if (mMetadataType == Schemaless || mMetadataType == Both) {
+    if (mSchemalessTU.isEnabled()) {
         
         MConn* s_metadata_connections = new MConn[mSchemalessTU.mNumReaders];
         for (int i = 0; i < mSchemalessTU.mNumReaders; i++) {
@@ -141,7 +173,7 @@ void Notifier::setup() {
         }
 
         mSchemalessMetadataReader = new SchemalessMetadataReader(s_metadata_connections, mSchemalessTU.mNumReaders,
-                mHopsworksEnabled, mElasticSearch, mPDICache);
+                mHopsworksEnabled, mProjectsElasticSearch, mPDICache);
         mSchemalessMetadataBatcher = new SchemalessMetadataBatcher(mMetadataLogTailer,
                 mSchemalessMetadataReader, mSchemalessTU.mWaitTime, mSchemalessTU.mBatchSize);
     }
@@ -149,27 +181,28 @@ void Notifier::setup() {
     if (mHopsworksEnabled) {
         Ndb* ops_log_tailer_connection = create_ndb_connection(mMetaDatabaseName);
         mhopsworksOpsLogTailer = new HopsworksOpsLogTailer(ops_log_tailer_connection, mPollMaxTimeToWait, mBarrier,
-                mElasticSearch, mPDICache, mSchemaCache);
+                mProjectsElasticSearch, mPDICache, mSchemaCache);
     }
-    
-    Ndb* ndb_elastic_provenance_conn = create_ndb_connection(mDatabaseName);
-    mProvenancElasticSearch = new ProvenanceElasticSearch(mElasticAddr, 
-            mElasticProvenanceIndex, mElasticProvenanceType, mElasticIssueTime, 
-            mElasticBatchsize, mStats,ndb_elastic_provenance_conn);
-     
-    
-    Ndb* provenance_tailer_connection = create_ndb_connection(mDatabaseName);
-    mProvenanceTableTailer = new ProvenanceTableTailer(provenance_tailer_connection, mPollMaxTimeToWait, mBarrier);
-    
-    SConn* provenance_connections = new SConn[mProvenanceTU.mNumReaders];
-    for(int i=0; i< mProvenanceTU.mNumReaders; i++){
-        provenance_connections[i] =  create_ndb_connection(mDatabaseName);
+
+    if (mProvenanceTU.isEnabled()) {
+        Ndb* ndb_elastic_provenance_conn = create_ndb_connection(mDatabaseName);
+        mProvenancElasticSearch = new ProvenanceElasticSearch(mElasticAddr,
+                mElasticProvenanceIndex, mElasticProvenanceType, mElasticIssueTime,
+                mElasticBatchsize, mStats, ndb_elastic_provenance_conn);
+
+
+        Ndb* provenance_tailer_connection = create_ndb_connection(mDatabaseName);
+        mProvenanceTableTailer = new ProvenanceTableTailer(provenance_tailer_connection, mPollMaxTimeToWait, mBarrier);
+
+        SConn* provenance_connections = new SConn[mProvenanceTU.mNumReaders];
+        for (int i = 0; i < mProvenanceTU.mNumReaders; i++) {
+            provenance_connections[i] = create_ndb_connection(mDatabaseName);
+        }
+        mProvenanceDataReader = new ProvenanceDataReader(provenance_connections, mProvenanceTU.mNumReaders,
+                mHopsworksEnabled, mProvenancElasticSearch, mPDICache);
+        mProvenanceBatcher = new ProvenanceBatcher(mProvenanceTableTailer, mProvenanceDataReader,
+                mProvenanceTU.mWaitTime, mProvenanceTU.mBatchSize);
     }
-    mProvenanceDataReader = new ProvenanceDataReader(provenance_connections,mProvenanceTU.mNumReaders,
-            mHopsworksEnabled, mProvenancElasticSearch, mPDICache);
-    mProvenanceBatcher = new ProvenanceBatcher(mProvenanceTableTailer, mProvenanceDataReader, 
-            mProvenanceTU.mWaitTime, mProvenanceTU.mBatchSize);
-   
 }
 
 Ndb_cluster_connection* Notifier::connect_to_cluster(const char *connection_string) {
